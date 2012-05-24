3219be511cf24274ee6f3a2c9fbe1c6fe4b10d18
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-05-24 10:26:22 +0200

    fixed script

--- bin/sync_deployRestlet.sh
+++ bin/sync_deployRestlet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "==== Building restlet ====\n";
 cd ~/syncProject/restlet;
-build;
+ant build;
 
 echo "==== Deploying restlet ====\n";
 cp dist/syncRestlet.war /var/lib/tomcat6/webapps/;
