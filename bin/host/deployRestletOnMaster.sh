1c3adbcb7ad971e664c9df13db5eb122e30831c5
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-05-25 14:04:10 +0200

    fixed wrong ssh call

--- bin/host/deployRestletOnMaster.sh
+++ bin/host/deployRestletOnMaster.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ssh master:/home/lrd900/syncProject/bin/master/pullGit.sh;
-ssh master:/home/lrd900/syncProject/bin/master/deployRestlet.sh;
+ssh master /home/lrd900/syncProject/bin/master/pullGit.sh;
+ssh master /home/lrd900/syncProject/bin/master/deployRestlet.sh;
