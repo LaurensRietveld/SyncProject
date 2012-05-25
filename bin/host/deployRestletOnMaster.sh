2308f71f4f08ec368b530c4ba44fb87b03cb31f7
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-05-25 13:59:14 +0200

    restructured bin files

---
+++ bin/host/deployRestletOnMaster.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ssh master:/home/lrd900/syncProject/bin/master/pullGit.sh;
+ssh master:/home/lrd900/syncProject/bin/master/deployRestlet.sh;
---
+++ bin/master/deployRestlet.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "==== Building restlet ====\n";
+cd ~/syncProject/restlet;
+ant;
+
+echo "==== Deploying restlet ====\n";
+cp dist/syncRestlet.war /var/lib/tomcat6/webapps/;
