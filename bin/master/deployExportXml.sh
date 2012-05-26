40db8debcc97aa856b7346e4cf30793f0626c079
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-05-26 23:16:25 +0200

    changed scripts to build on host, instead of each node separately

--- bin/master/deployExportXml.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "==== Building runnable jar for sesame export XML ====\n";
-cd ~/syncProject/sesameExport;
-ant;
-
-echo "==== Moving to desired location ====\n";
-cp dist/sesameExport.jar /usr/local/share/syncProject/;
-echo "done";
--- bin/master/deployRestlet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "==== Building restlet ====\n";
-cd ~/syncProject/restlet;
-ant;
-
-echo "==== Deploying restlet ====\n";
-cp dist/syncRestlet.war /var/lib/tomcat6/webapps/;
-echo "done";
