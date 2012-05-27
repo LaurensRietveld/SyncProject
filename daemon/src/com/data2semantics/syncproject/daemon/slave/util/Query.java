cad14a986c44309cf468340ae9f2dae79a429e07
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-05-27 21:30:05 +0200

    fixed indentation added print stacktrace for exception

--- daemon/src/com/data2semantics/syncproject/daemon/slave/util/Query.java
+++ daemon/src/com/data2semantics/syncproject/daemon/slave/util/Query.java
@@ -32,25 +32,31 @@ public class Query {
         PostMethod method = new PostMethod(uri);
         method.addParameter("update", query);
 
-        try{
-          int returnCode = client.executeMethod(method);
-
-          if(returnCode == HttpStatus.SC_NOT_IMPLEMENTED) {
-            System.err.println("The Post method is not implemented by this URI");
-            // still consume the response body
-            method.getResponseBodyAsString();
-          } else {
-            br = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream()));
-            String readLine;
-            while(((readLine = br.readLine()) != null)) {
-              System.err.println(readLine);
-            }
-          }
-        } catch (Exception e) {
-          System.err.println(e);
-        } finally {
-          method.releaseConnection();
-          if(br != null) try { br.close(); } catch (Exception fe) {}
-        }
+		try {
+			int returnCode = client.executeMethod(method);
+			if (returnCode == HttpStatus.SC_NOT_IMPLEMENTED) {
+				System.err.println("The Post method is not implemented by this URI");
+				// still consume the response body
+				method.getResponseBodyAsString();
+			} else {
+				br = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream()));
+				String readLine;
+				while (((readLine = br.readLine()) != null)) {
+					System.err.println(readLine);
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		} finally {
+			method.releaseConnection();
+			if (br != null) {
+				try {
+					br.close();
+				} catch (Exception fe) {
+					fe.printStackTrace();
+					System.exit(1);
+				}
+			}
+		}
 	}
 }
