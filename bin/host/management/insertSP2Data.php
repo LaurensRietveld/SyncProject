cd58a47378eae4f4059844831a4445e8bb3f8ba3
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-05-30 21:03:26 +0200

    fixed usage of wrong setting from config

--- bin/host/management/insertSP2Data.php
+++ bin/host/management/insertSP2Data.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 	//$jsonString = var_export(file_get_contents("../../config/config.conf"));
-	include_once(__DIR__."/../..//util.php");
+	include_once(__DIR__."/../../util.php");
 	$config = getConfig();
 	$numberOfTriples = 1000;
 	$dir = __DIR__."/../SP2Benchmark";
@@ -12,7 +12,7 @@
 		echo "\tGenerating file\n";
 		shell_exec("cd ".$dir." && sp2b_gen -t ".$numberOfTriples." ".$turtleFile);
 	} else {
-		echo "\tSP2 file already exists. Using that one.";
+		echo "\tSP2 file already exists. Using that one.\n";
 	}
 
 	echo "\tImporting to slave\n";
@@ -23,4 +23,4 @@
 	echo "\tImporting to master\n";
 	//Could also use php curl, but commandline is easy:
 	//curl -T output.n3 -H "Content-Type: text/rdf+n3;charset=UTF-8" http://localhost:8080/openrdf-sesame/repositories/master/statements
-	shell_exec('curl --silent -T '.$turtleFile.' -H "Content-Type: text/rdf+n3;charset=UTF-8" '.$config['slave']['tripleStore']['importDataUri']);
+	shell_exec('curl --silent -T '.$turtleFile.' -H "Content-Type: text/rdf+n3;charset=UTF-8" '.$config['master']['tripleStore']['importDataUri']);
