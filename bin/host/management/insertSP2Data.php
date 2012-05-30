971c144b0ffa827f06b54128fe0250606d4b46b0
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-05-30 20:32:34 +0200

    only create new SP2 file if it doesnt exist

--- bin/host/management/insertSP2Data.php
+++ bin/host/management/insertSP2Data.php
@@ -3,21 +3,23 @@
 	//$jsonString = var_export(file_get_contents("../../config/config.conf"));
 	include_once(__DIR__."/../..//util.php");
 	$config = getConfig();
-	
-	echo "==== Processing SP2Benchmark turtle file ====\n";
-	echo "\tGenerating file\n";
 	$numberOfTriples = 1000;
 	$dir = __DIR__."/../SP2Benchmark";
 	$turtleFile = __DIR__."/output.n3";
-	shell_exec("cd ".$dir." && sp2b_gen -t ".$numberOfTriples." ".$turtleFile);
+	
+	echo "==== Processing SP2Benchmark turtle file ====\n";
+	if (!file_exists($turtleFile)) {
+		echo "\tGenerating file\n";
+		shell_exec("cd ".$dir." && sp2b_gen -t ".$numberOfTriples." ".$turtleFile);
+	} else {
+		echo "\tSP2 file already exists. Using that one.";
+	}
 
-	echo "==== Importing to slave ====\n";
 	echo "\tImporting to slave\n";
 	//Could also use php curl, but commandline is easy:
 	//curl -T output.n3 -H "Content-Type: text/rdf+n3;charset=UTF-8" http://localhost:8080/openrdf-sesame/repositories/master/statements
 	shell_exec('curl --silent -T '.$turtleFile.' -H "Content-Type: text/rdf+n3;charset=UTF-8" '.$config['slave']['tripleStore']['importDataUri']);
 
-	echo "==== Importing to master ====\n";
 	echo "\tImporting to master\n";
 	//Could also use php curl, but commandline is easy:
 	//curl -T output.n3 -H "Content-Type: text/rdf+n3;charset=UTF-8" http://localhost:8080/openrdf-sesame/repositories/master/statements
