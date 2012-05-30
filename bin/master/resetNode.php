83761a6b9805a070d301fc19e36071a8ababeb8e
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-05-30 15:19:04 +0200

    fixed some indentation

--- bin/master/resetNode.php
+++ bin/master/resetNode.php
@@ -7,23 +7,23 @@
 	deleteDirContent("/usr/local/share/syncProject/logs");
 	echo "==== ".basename(__DIR__).": Emptying querylog dir ====\n";
 	deleteDirContent($config['master']['queryLogDir']);
-	echo "==== ".basename(__DIR__).":  Emptying XML dump dir ====\n";
+	echo "==== ".basename(__DIR__).": Emptying XML dump dir ====\n";
 	deleteDirContent($config['master']['xmlDumpDir']);
-	echo "==== ".basename(__DIR__).":  Cleaning GIT (including commit/push) ====\n";
+	echo "==== ".basename(__DIR__).": Cleaning GIT (including commit/push) ====\n";
 	$gitDir = $config['master']['gitDir']."/".$config['mode4']['repoDir'];
 	foreach (scandir($gitDir) as $item) {
 		if ($item == '.' || $item == '..' || $item == '.git') continue;
 		unlink($gitDir.DIRECTORY_SEPARATOR.$item);
 	}
 	`cd $gitDir; git pull -q;git add .; git commit -qm "cleaning dir"; git push -q origin master`;
-	echo "==== ".basename(__DIR__).":  Cleaning DB ====\n";
+	echo "==== ".basename(__DIR__).": Cleaning DB ====\n";
 	mysql_connect("localhost:3306", "syncProject");
 	$db = mysql_connect("localhost:3306", "syncProject");
 	if (!$db) die('Could not connect: ' . mysql_error());
 	mysql_select_db("QueryLog");
 	mysql_query("TRUNCATE TABLE `QueryLog`");
 	
-	echo "==== ".basename(__DIR__).":  Emptying triple store ====\n";
+	echo "==== ".basename(__DIR__).": Emptying triple store ====\n";
 	$uri = $config['master']['tripleStore']['clearStoreUri'];
 	$fields = array('context' => '');
 	doPost($uri, $fields);
--- bin/slave/resetNode.php
+++ bin/slave/resetNode.php
@@ -15,14 +15,14 @@
 	if (file_exists($executedQueries)) {
 		unset($executedQueries);
 	}
-	echo "==== ".basename(__DIR__).":  Cleaning DB (only 'already executed' DB. Run master beforehand to empty main QueryLog table) ====\n";
+	echo "==== ".basename(__DIR__).": Cleaning DB (only 'already executed' DB. Run master beforehand to empty main QueryLog table) ====\n";
 	mysql_connect("localhost:3306", "syncProject");
 	$db = mysql_connect("localhost:3306", "syncProject");
 	if (!$db) die('Could not connect: ' . mysql_error());
 	mysql_select_db("QueryLog");
 	mysql_query("TRUNCATE TABLE `ExecutedOnSlave`");
 
-	echo "==== ".basename(__DIR__).":  Emptying triple store ====\n";
+	echo "==== ".basename(__DIR__).": Emptying triple store ====\n";
 	$uri = $config['slave']['tripleStore']['clearStoreUri'];
 	$fields = array('context' => '');
 	doPost($uri, $fields);
