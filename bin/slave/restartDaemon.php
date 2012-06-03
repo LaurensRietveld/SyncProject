5c555833e203163f002e861504f58b25b106de69
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-06-03 16:01:40 +0200

    added running daemon as actual daemon

--- bin/slave/restartDaemon.php
+++ bin/slave/restartDaemon.php
@@ -3,14 +3,21 @@
 	include(__DIR__."/../util.php");
 	$config = getConfig();
 	$result = shell_exec("ps -C java");
-	//$result = "19073 pts/2    00:00:01 j";
+	$mode = $argv[1];
+	if ($mode <= 0 || $mode > 6 ) {
+		echo "No mode number passed as parameter. Stopping\n";
+		exit;
+	}
+	
 	preg_match("/\s*(\d*)\spts.*/", $result, $matches);
 	$pid = (int)$matches[1];
 	if ($pid > 0) {
 		echo "==== Restarting Daemon ====\n";
 		//Daemon is running
-		shell_exec("kill " + $pid);
+		shell_exec("kill ".$pid);
 	} else {
 		echo "==== Starting Daemon ====\n";
+		
 	}
 	
+	$result = shell_exec("java -jar /usr/local/share/syncProject/daemon.jar --mode ".$mode." > /usr/local/share/syncProject/logs/daemon.txt &");
