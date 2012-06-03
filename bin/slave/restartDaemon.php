824ec8087c98146445bcd71af5c3e413ff04ce5f
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-06-03 16:19:57 +0200

    finished implementation for restarting daemons remotely

--- bin/slave/restartDaemon.php
+++ bin/slave/restartDaemon.php
@@ -2,22 +2,20 @@
 <?php
 	include(__DIR__."/../util.php");
 	$config = getConfig();
-	$result = shell_exec("ps -C java");
+	$result = shell_exec("ps axuwww | grep java | grep -v grep | grep daemon.jar");
 	$mode = $argv[1];
 	if ($mode <= 0 || $mode > 6 ) {
 		echo "No mode number passed as parameter. Stopping\n";
 		exit;
 	}
-	
-	preg_match("/\s*(\d*)\spts.*/", $result, $matches);
-	$pid = (int)$matches[1];
-	if ($pid > 0) {
+
+	preg_match_all("/\s*lrd900\s*(\d*).*/", $result, $matches);
+	if (is_array($matches[1])) {
 		echo "==== Restarting Daemon ====\n";
-		//Daemon is running
-		shell_exec("kill ".$pid);
+		foreach ($matches[1] as $match) {
+			shell_exec("kill ".(int)$match);
+		}
 	} else {
 		echo "==== Starting Daemon ====\n";
-		
 	}
-	
 	$result = shell_exec("java -jar /usr/local/share/syncProject/daemon.jar --mode ".$mode." > /usr/local/share/syncProject/logs/daemon.txt &");
