f23feb43195fca1fda1147197b794ddf01276a43
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-06-04 17:39:53 +0200

    added timing info

--- bin/slave/restartDaemon.php
+++ bin/slave/restartDaemon.php
@@ -8,14 +8,14 @@
 		echo "No mode number passed as parameter. Stopping\n";
 		exit;
 	}
-
+	echo "==== ".date("Ymd H:i:s").": ";
 	preg_match_all("/\s*lrd900\s*(\d*).*/", $result, $matches);
 	if (is_array($matches[1])) {
-		echo "==== Restarting Daemon ====\n";
+		echo "Restarting Daemon ====\n";
 		foreach ($matches[1] as $match) {
 			shell_exec("kill ".(int)$match);
 		}
 	} else {
-		echo "==== Starting Daemon ====\n";
+		echo "Starting Daemon ====\n";
 	}
 	$result = shell_exec("java -jar /usr/local/share/syncProject/daemon.jar --mode ".$mode." > /usr/local/share/syncProject/logs/daemon.txt &");
