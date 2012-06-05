ed75766b76602103217b74134fdafa10f738d926
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-06-06 00:10:46 +0200

    added uuuggglllyyy workaround to get the bloody tcp logs

---
+++ bin/slave/TcpDumpDaemon.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+	include(__DIR__."/../util.php");
+	$config = getConfig();
+	$startTcp = $config['experiments']['experimentCacheDir']."/startTcpDump";
+	$stopTcp = $config['experiments']['experimentCacheDir']."/stopTcpDump";
+	$storeFile = $config['experiments']['experimentCacheDir']."/tcpDump/s_dump.txt";
+	while (true) {
+		//check whether I need to start daemon
+		usleep(500000); //sleep 0.5 sec
+		if (file_exists($startTcp)) {
+			startDaemon($storeFile);
+			unlink($startTcp);
+		}
+		if (file_exists($stopTcp)) {
+			stopDaemon();
+			unlink($stopTcp);
+		}
+	}
+	
+	function stopDaemon() {
+		shell_exec("/home/lrd900/gitCode/bin/slave/stopTcpDump.php");
+	}
+	
+	function startDaemon($storeInFile) {
+		$cmd = "sudo tcpdump -nq -i eth1 \(\(src host 192.168.56.122 or src host 192.168.56.133\) and \(dst host 192.168.56.122 or dst host 192.168.56.133\)\) >> ".$storeInFile." &";
+		shell_exec($cmd);
+		echo "\tStarted tcpdump\n";
+	}
+
