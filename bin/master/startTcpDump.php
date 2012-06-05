14b5d8ac362e7ce96e6e463bfb3040b978ac66ab
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-06-05 22:21:43 +0200

    fixed scripts to start tcp dump

--- bin/master/startTcpDump.php
+++ bin/master/startTcpDump.php
@@ -7,6 +7,7 @@
 	if (!$storeInFile) {
 		echo "Need to pass filename as parameter, to which results are stored";
 	}
+	echo $storeInFile;
  	$cmd = "sudo tcpdump -nq -i eth1 >> ".$storeInFile." &";
 	shell_exec($cmd);
 	echo "\tStarted tcpdump as daemon\n";
