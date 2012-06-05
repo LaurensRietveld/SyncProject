39cc5e78af1f277f4115df27b5769bd111649b07
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-06-05 21:39:20 +0200

    added script for tcpdump starting

---
+++ bin/slave/startTcpDump.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+	include(__DIR__."/../../util.php");
+	$config = getConfig();
+	
+	$storeInFile = $argv[1];
+	if (!$storeInFile) {
+		echo "Need to pass filename as parameter, to which results are stored";
+	}
+	echo $storeInFile;exit;
+ 	$cmd = "sudo tcpdump -nq -i vboxnet0 >> ".$storeInFile." &";
+	shell_exec($cmd);
+	echo "\tStarted tcpdump as daemon\n";
