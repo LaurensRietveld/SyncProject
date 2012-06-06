00696ac6c3c45874e7843383f66fc7c3b61cb1fc
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-06-06 18:35:04 +0200

    set less permissions, as otherwise it creates conflict for the git files (as permissions are also committed)

--- bin/slave/setPermissions.sh
+++ bin/slave/setPermissions.sh
@@ -1,1 +1,4 @@
-../master/setPermissions.sh
+#!/bin/sh
+sudo chown -R lrd900:syncProject /usr/local/share/syncProject
+#sudo chmod -R 775 /usr/local/share/syncProject
+#sudo chown -R tomcat6:syncProject /usr/local/share/syncProject/git/repo
